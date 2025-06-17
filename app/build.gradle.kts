9898ce6f84dc81c0f14844fd272d1719ba462def
Adan Gabriel Balbuena Luna <agbalbuenal@liverpool.com.mx>
2025-06-17 11:24:03 -0600

    [CONFIGURATION-PROJECT]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -45,4 +48,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    //Retrofit Converter
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    //Corrutinas
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.6")
 }
